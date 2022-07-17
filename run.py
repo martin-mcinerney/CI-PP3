c46e93478af263d8850414e948e833846856fc5e
martin-mcinerney <martin_mcinerney@live.ie>
2022-07-17 16:32:15 +0100

    Adds show_board function

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def show_board(hit, miss, comp): 
+    print('           Battleships\n')
+    print('     0  1  2  3  4  5  6  7  8  9')
+
+    place = 0
+    for x in range(10):
+        row = ''
+        for y in range(10):
+            ch = ' _ '
+            if place in miss:
+                ch = ' x '
+            elif place in hit:
+                ch = ' o '
+            elif place in comp:
+                ch = ' O '
+            
+            row = row + ch
+            place = place + 1
+        print(x, " ", row)
+
+hit = [23,24,25]
+miss = [45, 32, 76]
+comp = [1,2]
+
+show_board(hit, miss, comp )
